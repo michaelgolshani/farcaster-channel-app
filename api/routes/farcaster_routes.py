cbf1b4a3f82aeefcb11829184ebace45af0fb68b
Michael Golshani <michaelgolshani@gmail.com>
2024-05-10 10:44:04 -0700

    Created GET route to retrieve all channels with their details.

--- api/routes/farcaster_routes.py
+++ api/routes/farcaster_routes.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, request, jsonify
+import requests
+import os
 
 farcaster_routes = Blueprint("farcaster_routes", __name__)
 
@@ -7,3 +9,23 @@ farcaster_routes = Blueprint("farcaster_routes", __name__)
 def ping():
     return {"message": "pong"}, 200
 
+
+# create a path that will save the trending channels in the farcaster database to our database
+@farcaster_routes.route('/trending_channels', methods=['GET'])
+def get_trending_channels():
+    """Get the trending channels from the farcaster database and save them to our database"""
+
+    URL = "https://api.neynar.com/v2/farcaster/channel/list?limit=50"
+    API_KEY = os.environ.get("NEYNAR_API_KEY")
+    headers = {
+        "accept": "application/json",
+        "api_key": f'{API_KEY}'
+    }
+
+    response = requests.get(URL, headers=headers, timeout=5)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data, 200
+    else:
+        return {"message": "Unable to get trending channels"}, 500
--- api/routes/hello_world_routes.py
+++ api/routes/hello_world_routes.py
@@ -6,4 +6,4 @@ hello_world_routes = Blueprint("hello_world_routes", __name__)
 
 @hello_world_routes.route("/")
 def hello_world():
-    return {"message": "Hello, World TEST3!"}, 200
+    return {"message": "Hello, World TEST!"}, 200
